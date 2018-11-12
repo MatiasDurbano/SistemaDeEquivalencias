928f52d0d7df6a2bd37182d230557b20a263a476
Maxi Pompilio <maxi.pompilio@gmail.com>
2018-11-12 17:27:15 -0300

    nuevo

--- src/app/ModuloSolicitud/clases/Solicitud.ts
+++ src/app/ModuloSolicitud/clases/Solicitud.ts
@@ -5,13 +5,12 @@ import { Comentario } from './Comentario';
 export class Solicitud {
     alumno: Alumno;
     asignaturasUNGS: Array<AsignaturaUNGS>;
-    estado: string;
-    comentario: Comentario
+    estado: boolean;
+
     constructor(alumno: Alumno, asignaturasUNGS: Array<AsignaturaUNGS>) {
         this.alumno = alumno;
         this.asignaturasUNGS = asignaturasUNGS;
-        this.estado = "En_Espera";
-        this.comentario=new Comentario();
+        this.estado = false;
     }
 
     saludar(): string {
