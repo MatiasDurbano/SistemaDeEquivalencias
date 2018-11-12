0cd6ae0f63529a1534d43065dbb2769db514cd4e
MatiasDurbano <matiasgabrieldurbano@gmail.com>
2018-11-12 17:26:50 -0300

    commit final

--- src/app/ModuloSolicitud/clases/Solicitud.ts
+++ src/app/ModuloSolicitud/clases/Solicitud.ts
@@ -6,12 +6,12 @@ export class Solicitud {
     alumno: Alumno;
     asignaturasUNGS: Array<AsignaturaUNGS>;
     estado: string;
-    comentario: Comentario
+    
     constructor(alumno: Alumno, asignaturasUNGS: Array<AsignaturaUNGS>) {
         this.alumno = alumno;
         this.asignaturasUNGS = asignaturasUNGS;
         this.estado = "En_Espera";
-        this.comentario=new Comentario();
+        
     }
 
     saludar(): string {
